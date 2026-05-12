78242b9078501aedfbf9dcbeae5f2be4a81bd6fb
Nixells <yvlad4803@gmail.com>
2026-05-12 10:56:47 +0200

    d

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const log = document.querySelector(".buttone");
+const gallera = document.querySelector(".gallery");
+
+log.addEventListener("click", () => {
+  gallera.classList.toggle("show");
+});
